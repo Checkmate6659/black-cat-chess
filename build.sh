beed108179361538a5264f43379aa1abed867c06
EnigmaChess6659 <73489554+Checkmate6659@users.noreply.github.com>
2022-08-30 19:16:21 +0200

    Added test position for leaper movement

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ ls
 # build/black_cat.out
 make ; build/black_cat.out
 
+#test incremental attack tables using this: position fen 8/5pp1/2n5/8/3k4/8/PPP5/2K5 w - - 0 1
 #test TT using this (Lasker Reichhelm): position fen 8/k7/3p4/p2P1p2/P2P1P2/8/8/K7 w - -
 #mate in 3 (HAKMEM 70): position fen 5B2/6P1/1p6/8/1N6/kP6/2K5/8 w - - 0 1
 #WARNING: NMH makes engine fail HAKMEM 70 (stuck on mate in 4 with g8=Q; although shouldn't lower playing strength too much); although it might be because mate scores are stored incorrectly in tt
